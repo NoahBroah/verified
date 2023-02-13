52c690db42a8c4f9200d0abde37d52db03305139
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-12 22:55:27 -0800

    Jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,48 @@
 class JobsController < ApplicationController
-end
+    before_action :authorize, only: [:create, :update, :destroy]
+  
+    def index
+      jobs = Job.all
+      render json: jobs, status: :ok
+    end
+  
+    def show
+      job = Job.find_by(id: params[:id])
+      render json: job, status: :ok
+    end
+  
+    def create
+      job = current_user.jobs.create(job_params)
+      if job.valid?
+        render json: job, status: :created
+      else
+        render json: { errors: job.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      job = Job.find_by(id: params[:id])
+      if job.user_id == current_user.id
+        job.update(job_params)
+        render json: job, status: :ok
+      else
+        render json: { errors: ["Not Authorized"] }, status: :unauthorized
+      end
+    end
+  
+    def destroy
+      job = Job.find_by(id: params[:id])
+      if job.user_id == current_user.id
+        job.destroy
+        head :no_content
+      else
+        render json: { errors: ["Not Authorized"] }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def job_params
+      params.require(:job).permit(:company, :position, :job_duties)
+    end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authorize
     def index
         users = User.all 
         render json: users, status: :ok
