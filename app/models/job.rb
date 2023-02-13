894f997e05f5a789ba64e54566900e67c7933223
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-12 19:35:17 -0800

    Job Model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :user
+  has_many :verifications, dependent: :destroy
+  has_many :verifying_users, through: :verifications, source: :user
+
+  validates :company, :position, presence: true
 end
